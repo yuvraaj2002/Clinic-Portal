0424d4861840e7774f3da904612333fd36306b30
yuvraaj2002 <ys2002github@gmail.com>
2025-08-02 21:39:04 +0530

    upDATED THE CREDENTIALS

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,6 @@ export default {
 	],
 	theme: {
 		extend: {
-			transitionTimingFunction: {
-				'ease-in-out': 'cubic-bezier(0.4, 0, 0.2, 1)',
-			},
 			colors: {
 				'primary': {
 					50: '#f3f0ff',
